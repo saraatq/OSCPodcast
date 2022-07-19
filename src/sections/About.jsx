a6fbf1137bb4813befef1416284ed034de38c286
ahmed <ahmednigm111@gmail.com>
2022-07-19 16:50:58 +0200

    about_v1

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import bg1 from '../AboutImgs/p1.jpg'
+import bg2 from '../AboutImgs/p2.jpg'
+import googlelogo from '../AboutImgs//logos/google.png'
+import applelogo from '../AboutImgs//logos/apple.png'
+import soundlogo from '../AboutImgs//logos/sound.png'
+import spotifylogo from '../AboutImgs//logos/spotify.png'
+import sop from '../'
 import './About.css'
 const About = () => {
   return (
@@ -6,8 +13,8 @@ const About = () => {
         <div className='wrapper'>
             <div className='left'>
                 <div className='imgwrapper'>
-                <img className='img1' />
-                <img className='img2' />
+                <img className='img1' src={bg1} />
+                <img className='img2' src={bg2} />
                 </div>
             </div>
             <div className='right'>
@@ -28,10 +35,10 @@ const About = () => {
             <h6 className='footerh'>Prodcasts available on </h6>
             
             <ul className='footerul'>
-                <li className='footerli'><a className='logolink' href='#'><img className='footerlogos' /></a></li>
-                <li className='footerli'><a className='logolink' href='#'><img className='footerlogos'  /></a></li>
-                <li className='footerli'><a className='logolink'  href='#'><img className='footerlogos'  /></a></li>
-                <li className='footerli'><a className='logolink'  href='#'><img className='footerlogos'  /></a></li>
+                <li className='footerli'><a className='logolink' href='#'><img className='footerlogos' src={applelogo} /></a></li>
+                <li className='footerli'><a className='logolink' href='#'><img className='footerlogos' src={spotifylogo} /></a></li>
+                <li className='footerli'><a className='logolink'  href='#'><img className='footerlogos' src={googlelogo} /></a></li>
+                <li className='footerli'><a className='logolink'  href='#'><img className='footerlogos' src={soundlogo} /></a></li>
             </ul>
         </div>
     </div>
