fb7753df31c8619d9d203f7d374ce1fa3749f236
sara tareq <53131646+saraatq@users.noreply.github.com>
2022-07-25 18:14:34 +0200

    add new episodes section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 import {Header, About, NewEps, Subscribe} from './sections'
-// import Header from './sections/Header'
 import './App.css'
 
 const App = () => {
+  let new_eps = [{img:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRhyg5iFTy6-3IAfjyDMkvqxBlarPuYks7yQQAqLY-Qrs41c-I", url:"https://podcasts.google.com/?feed=aHR0cHM6Ly9mZWVkcy5zaW1wbGVjYXN0LmNvbS9wbExCSzhfXw%3D%3D&episode=YTQ4YWQyOGQtYmIwNy00ODQ2LTg5ZTMtOGNhYTdkNDMxOThh", name:"Episode[46]: Technical Hiring With Aya Elgebeely", author:"null++: بالعربي‎"},
+  {img:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRhyg5iFTy6-3IAfjyDMkvqxBlarPuYks7yQQAqLY-Qrs41c-I", url:"https://podcasts.google.com/?feed=aHR0cHM6Ly9mZWVkcy5zaW1wbGVjYXN0LmNvbS9wbExCSzhfXw%3D%3D&episode=ZDA4ODQxNGEtYWIzYS00M2I0LTgzZjktNzZiMGYwODQ3NWFm", name:"Episode[54]: Single Page Apps, Server Side Rendering & Static Site Generators", author:"null++: بالعربي‎"},
+  {img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpPa8yGO2jBfXgfBIbD1duiqhhQjnHC-lJAKdlNZsUJHpZm-qK", url:"https://podcasts.google.com/?feed=aHR0cHM6Ly9mZWVkcy5mZWVkYnVybmVyLmNvbS9Bc2tkZXZlbG9wZXI%3D&episode=dGFnOnNvdW5kY2xvdWQsMjAxMDp0cmFja3MvODU2MDM4MzEw", name:"EP60 - AskDeveloper Podcast - #لايف_مع_القهوة و حنجيب سيرة المهارات الأساسية للجونيور النهاردة‎", author:"AskDeveloper Podcast"},
+  {img:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSVTPAyrcDAearNg0xnrtlIGv3np014iTawe3tul0qFaijuYQA", url:"https://podcasts.google.com/?feed=aHR0cHM6Ly9sZXhmcmlkbWFuLmNvbS9mZWVkL3BvZGNhc3Qv&episode=aHR0cHM6Ly9sZXhmcmlkbWFuLmNvbS8_cD00OTM0", name:"#267 – Mark Zuckerberg: Meta, Facebook, Instagram, and the Metaverse", author:"Lex Fridman Podcast"}]
+
   return (
     <div className='app'>
       <div className='header'>
@@ -14,7 +18,7 @@ const App = () => {
         <About/>
       </div>
       <div className='neweps'>
-        <NewEps/>
+        <NewEps new_eps = {new_eps}/>
       </div>
       <div className='subscribe'>
         <Subscribe/>
--- src/sections/NewEps.jsx
+++ src/sections/NewEps.jsx
@@ -1,11 +1,37 @@
-// rafce -> react snippet
-
 import React from 'react'
+import './NewEps.css'
+import play from '../assets/play-button.png'
 
-const NewEps = () => {
+const NewEps = (props) => {
+  // 3-> newest   0-> oldest
   return (
-    <div>NewEps</div>
+    <>
+    <h1 class="ep-header">NEW EPISODES</h1>
+    <div  class="grid-container">
+      <div class ="ep3">
+        <img class = "ep-img" src={props.new_eps[3].img} alt ={props.new_eps[3].name} ></img>
+        <h3 class = "ep-title" >{props.new_eps[3].name}</h3>
+        <a href={props.new_eps[3].url} target= "_blank" ><button type="button" class = "play-btn btn-alone"><img src={play} alt="play episode" ></img></button></a> 
+      </div> 
+      <div class ="ep2">
+        <img class = "ep-img" src={props.new_eps[2].img} alt ={props.new_eps[2].name} ></img>
+        <a href={props.new_eps[2].url} target= "_blank" ><button type="button" class = "play-btn"><img src={play} alt="play episode" ></img></button> </a>
+        <h3 class = "ep-title" >{props.new_eps[2].name}</h3>
+      </div> 
+      <div class ="ep1">
+        <img class = "ep-img" src={props.new_eps[1].img} alt ={props.new_eps[1].name} ></img>
+        <a href={props.new_eps[1].url} target= "_blank" ><button type="button" class = "play-btn"><img src={play} alt="play episode" ></img></button></a>
+        <h3 class = "ep-title" >{props.new_eps[1].name}</h3>
+      </div> 
+      <div class ="ep0">
+        <img class = "ep-img" src={props.new_eps[0].img} alt ={props.new_eps[0].name} ></img>
+        <a href={props.new_eps[0].url} target= "_blank" ><button type="button" class = "play-btn"><img src={play} alt="play episode" ></img></button></a>
+        <h3 class = "ep-title" >{props.new_eps[0].name}</h3>
+      </div> 
+    </div>
+    </>
   )
+
 }
 
 export default NewEps
